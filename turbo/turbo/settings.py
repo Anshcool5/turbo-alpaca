8c798de17e9378e59bd20c0b2248199aadce0964
SidMcStarter <dsiddharth2004@gmail.com>
2025-02-15 12:41:02 -0700

    updated tracked

--- turbo/turbo/settings.py
+++ turbo/turbo/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hr0$t$i&-s2y7^=h$+&c90(o$k(v3qr^0i(3f^ovrfq_#f8!7)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
