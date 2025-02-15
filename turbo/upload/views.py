f2e23c5039acc4d50d0648b990bf3a67998cfb86
SidMcStarter <dsiddharth2004@gmail.com>
2025-02-15 12:19:37 -0700

    basic setup

---
+++ turbo/upload/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+#import httpresponse
+from django.http import HttpResponse
+# Create your views here.
+def upload(request):
+    return HttpResponse("Hey nerds!!!")
+    
