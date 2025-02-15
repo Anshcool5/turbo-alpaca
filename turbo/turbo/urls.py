5fefe90903c86363f32b0a333a6443f3541fdb6b
Ishaan Meena <ishhaan.m@gmail.com>
2025-02-15 14:56:58 -0700

    removed upload.html/added base html , reformatted views.py + urls.py

--- turbo/turbo/urls.py
+++ turbo/turbo/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from upload import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("upload.urls")),  # Corrected line
+    path('', views.home, name='home'),
+    path("", include("upload.urls")),
 ]
