fff817059bff9f23fcca1caa63f5a8426073e7d1
melritac <melritacyriac123@gmail.com>
2025-02-16 11:51:13 -0700

    querying documents functionality

--- turbo/upload/urls.py
+++ turbo/upload/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     #path('', views.home, name='home'),  # Home page
     path('upload/', views.upload_file, name='upload_file'),  # File upload view
 
+
     # Authentication views
     path('home/', views.home, name='home'),  # Home page
     path('register/', views.register, name='register'),  # User registration
     path('login/', views.user_login, name='login'),  # User login
     path('logout/', views.user_logout, name='logout'),  # User logout
+    path("query_documents/", views.query_documents, name="query_documents"),
 ]
