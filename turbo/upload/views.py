cd89a6f546a23b84885f42b64b486ff9a8edd617
melritac <melritacyriac123@gmail.com>
2025-02-15 20:43:43 -0700

    logging out mechanism

--- turbo/upload/views.py
+++ turbo/upload/views.py
@@ -131,7 +131,7 @@ def user_login(request):
 # User logout view
 def user_logout(request):
     logout(request)
-    return redirect("home")  # Redirect to the home page
+    return redirect("login")  # Redirect to the home page
 
 
 def query_pinecone(request):
