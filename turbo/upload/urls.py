67fbe1f0f037a1361b237c6e2bc241772b45d364
melritac <melritacyriac123@gmail.com>
2025-02-17 07:04:12 -0700

    llama generate thingy

--- turbo/upload/urls.py
+++ turbo/upload/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
